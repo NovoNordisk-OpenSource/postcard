02740b243ff982db07464a43f74e4dcb756c6380
mathiaslj <Math964b@yahoo.dk>
2025-09-11 10:58:29 +0200

    tests/ci: Changed name of figure files to see if that fixes NOTE from R CMD

--- tests/testthat/test-repeat_power_plot.R
+++ tests/testthat/test-repeat_power_plot.R
@@ -11,7 +11,7 @@ test_that("`repeat_power_marginaleffect` works", {
 
   rpm <- repeat_power_marginaleffect(target_effect = 0.9, exposure_prob = 0.5, ns = seq(100, 500, 100))
   expect_snapshot(rpm)
-  expect_doppelganger("Vertical and horizontal lines meet where power curve crosses horizontal line - marginaleffect",
+  expect_doppelganger("plot_repeat_power_marginaleffect",
                       plot(rpm))
 })
 
@@ -28,6 +28,6 @@ test_that("`repeat_power_linear` works", {
     formula_list = list("ANCOVA 1 covariate" = Y ~ X, "ANCOVA 2 covariates" = Y ~ W + X),
     train_data = train_data)
   expect_snapshot(rpl)
-  expect_doppelganger("Vertical and horizontal lines meet where power curve crosses horizontal line - linear",
+  expect_doppelganger("plot_repeat_power_linear",
                       plot(rpl))
 })
